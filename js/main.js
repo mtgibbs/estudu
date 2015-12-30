306b6b6a94884a60dcd11759f9a7b79595b9f090
Matt Gibbs <mtgibbs21@gmail.com>
2015-12-30 12:05:03 -0500

    Extracted out inline animation script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/*global tau*/
+/*global widget*/
+/*global $*/
+
 var IFTTT_URL = 'https://maker.ifttt.com/trigger/{event}/with/key/{key}';
 var iftttKey = widget.preferences.getItem('ifttt_key');
 
@@ -13,23 +17,58 @@ window.onload = function() {
 				} catch (ignore) {
 				}
 			} else {
-				window.history.back();
+				tau.back();
 			}
 		}
 	} );
 	
-	$('body').on('click', 'a[data-event]', function(e) {
+	// load page animations for new lists on creation
+	window.addEventListener('pagecreate', function() {
+		var page = document.getElementById("pageAnimation"),
+		listHelper,
+		elScroller;
+		
+		console.log(page.outerHTML);
+		console.log(page.innerHTML);
+
+		page.addEventListener("pagebeforeshow", function () {
+			var list;
+	
+			elScroller = page.querySelector(".ui-scroller");
+			if (elScroller) {
+				list = elScroller.querySelector(".ui-listview");
+			}
+	
+			if (elScroller && list) {
+				listHelper = tau.helper.SnapListStyle.create(list, {animate: "scale"});
+	
+				elScroller.setAttribute("tizen-circular-scrollbar", "");
+			}
+		});
+	
+		page.addEventListener("pagebeforehide", function () {
+			if (listHelper) {
+				listHelper.destroy();
+	
+				listHelper = null;
+	
+				if (elScroller) {
+					elScroller.removeAttribute("tizen-circular-scrollbar");
+				}
+			}
+		});
+	});
+	
+	$('body').on('click', 'a[data-event]', function() {
 		var event = $(this).data('event');
 		
 		var endpoint = IFTTT_URL.replace(/\{event\}/g, event);
 		endpoint = endpoint.replace(/\{key\}/g, iftttKey);
-		console.log(endpoint);
 		
 		$.ajax({
 			url: endpoint,
 			async: false,
-			success: function(data) {
-				console.log(data);
+			success: function() {
 				tau.back();
 			},
 			error: function(jqXHR, status, err) {
